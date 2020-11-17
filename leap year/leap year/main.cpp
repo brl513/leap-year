db71068e024620753a2e67a26d53e7c7bf0a2fed
brl513 <49693346+brl513@users.noreply.github.com>
2020-11-17 18:41:40 +0000

    Add files via upload

---
+++ leap year/leap year/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  leap year
+//
+//  Created by brl513 on 16/11/2020.
+//  Copyright © 2020 brl513. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    bool yearIs4Digits = false;
+    bool yearIsLeapYear = false;
+    while (yearIs4Digits == false || yearIsLeapYear == false){
+        int year;
+        std::cout << "Enter year: ";
+        std::cin >> year;
+        int count = 0;
+        int n = year;
+        while (n != 0){
+            n = n/10;
+            count = count + 1;
+        }
+        if (count == 4){
+            // std::cout << "Year is four-digits" << std::endl;
+            yearIs4Digits = true;
+            int a = year % 4;
+            int b = year % 100;
+            int c = year % 400;
+            if (a == 0) {
+                // std::cout << "Year could be a leap year." << std::endl;
+                if ((b == 0 && c == 0) || b != 0 ) {
+                    std::cout << "Year is a leap year." << std::endl;
+                    yearIsLeapYear = true;
+                }
+                else {
+                    std::cout << "Year is not a leap year." << std::endl;
+                }
+            }
+            else {
+                std::cout << "Year is not a leap year." << std::endl;
+            }
+        }
+        else {
+            std::cout << "You need to re-enter a four-digit year." << std::endl;
+        }
+        
+    }
+  
+}
